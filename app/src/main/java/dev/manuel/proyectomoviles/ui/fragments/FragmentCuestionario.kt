791e899bf11e92f95109da9d73487d70aadaab8d
Manuel Bautista <garcia.manuel.b7@gmail.com>
2022-10-29 21:11:04 -0500

    XD

--- app/src/main/java/dev/manuel/proyectomoviles/ui/fragments/FragmentCuestionario.kt
+++ app/src/main/java/dev/manuel/proyectomoviles/ui/fragments/FragmentCuestionario.kt
@@ -1,10 +1,11 @@
-package dev.manuel.proyectomoviles
+package dev.manuel.proyectomoviles.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import dev.manuel.proyectomoviles.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
