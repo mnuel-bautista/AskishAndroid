63e7939b4be6c81d572ae0525256467df2e002a4
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2022-11-15 13:47:36 -0600

    Primeros cambios de Grupos

--- app/src/main/java/dev/manuel/proyectomoviles/ui/fragments/FragmentGrupos.kt
+++ app/src/main/java/dev/manuel/proyectomoviles/ui/fragments/FragmentGrupos.kt
@@ -1,60 +1,71 @@
 package dev.manuel.proyectomoviles.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dev.manuel.proyectomoviles.R
+import dev.manuel.proyectomoviles.adapters.GrupoAdapter
+import dev.manuel.proyectomoviles.databinding.FragmentGruposBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentGrupos.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentGrupos : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var recycleView: RecyclerView
+    private val db = Firebase.firestore
+    private val nombre = ArrayList<String>()
+
+    private lateinit var binding: FragmentGruposBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_grupos, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentGrupos.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentGrupos().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    //CODIGO AGREGADO
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentGruposBinding.bind(view)
+
+        //Referenciar al layout del recycleview
+        recycleView = binding.recycleView
+        recycleView.layoutManager = LinearLayoutManager(requireContext())
+        recycleView.adapter = GrupoAdapter()
+
+        leer()
     }
+
+    private fun leer() {
+
+        //Mostrar los grupos a los que pertenece el usuario
+//        db.collection("grupos").whereEqualTo("codigo", "1122")
+//            .get()
+//            .addOnSuccessListener {
+//                it.documents.first().reference.update(mapOf("integrantes.asdfasdfasd" to true))
+//            }
+
+//        db.collection("grupos").whereEqualTo("integrantes.aSDFASDFasdfasd", true).addSnapshotListener{ value, e ->
+//            for (doc in value!!){
+//                doc.getString("nombre")?.let {
+//                    nombre.add(it)
+//                }
+//            }
+//            if (nombre.isNotEmpty()){
+//                (recycleView.adapter as GrupoAdapter).setListNames(nombre)
+//            }
+//            println("Array $nombre")
+//        }
+    }
+
 }
