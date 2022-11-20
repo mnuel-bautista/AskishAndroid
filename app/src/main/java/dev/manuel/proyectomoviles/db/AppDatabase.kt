146b250e67d793d98033ad6deb6bb6d488f82d48
Manuel Bautista <garcia.manuel.b7@gmail.com>
2022-11-20 14:11:52 -0600

    Agregar variables de entorno

--- app/src/main/java/dev/manuel/proyectomoviles/db/AppDatabase.kt
+++ app/src/main/java/dev/manuel/proyectomoviles/db/AppDatabase.kt
@@ -3,15 +3,21 @@ package dev.manuel.proyectomoviles.db
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import dev.manuel.proyectomoviles.BuildConfig
 
 class AppDatabase {
 
     val firestore = Firebase.firestore.apply {
-        useEmulator("192.168.0.32", 8080)
-        val settings = FirebaseFirestoreSettings.Builder()
-            .setPersistenceEnabled(false)
-            .build()
-        firestoreSettings = settings
+        val host = BuildConfig.HOST
+        val port = BuildConfig.PORT
+
+        if(host != "" && port != 0) {
+            useEmulator(host, port)
+            val settings = FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(false)
+                .build()
+            firestoreSettings = settings
+        }
     }
 
     companion object {
