01433ce6d4bc946cd5edf8071d330dc68a112aa4
Manuel Bautista <garcia.manuel.b7@gmail.com>
2022-11-27 15:00:18 -0600

    Liberar recursos cuando no son necesarios

--- app/src/main/java/dev/manuel/proyectomoviles/viewmodels/QuizRoomViewModel.kt
+++ app/src/main/java/dev/manuel/proyectomoviles/viewmodels/QuizRoomViewModel.kt
@@ -3,8 +3,12 @@ package dev.manuel.proyectomoviles.viewmodels
 import androidx.lifecycle.ViewModel
 import dev.manuel.proyectomoviles.repositories.QuizzRoomRepository
 
-class QuizRoomViewModel: ViewModel() {
+class QuizRoomViewModel(): ViewModel() {
 
     val repository = QuizzRoomRepository()
+
+    override fun onCleared() {
+        repository.removeListeners()
+    }
 
 }
