f18969e3f5ea010f923144121846844f7e936d3a
Angel1t0 <105956225+Angel1t0@users.noreply.github.com>
2022-11-28 06:12:05 -0600

    Automatizando idPreguntas

--- app/src/main/java/dev/manuel/proyectomoviles/ui/fragments/FragmentCuestionario.kt
+++ app/src/main/java/dev/manuel/proyectomoviles/ui/fragments/FragmentCuestionario.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +15,8 @@ import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
 import dev.manuel.proyectomoviles.R
 import dev.manuel.proyectomoviles.db.AppDatabase
+import dev.manuel.proyectomoviles.getUserId
+import dev.manuel.proyectomoviles.models.CuestionariosModel
 import dev.manuel.proyectomoviles.models.PreguntasModel
 import dev.manuel.proyectomoviles.ui.fragments.adapters.AdaptadorPreguntas
 
@@ -24,16 +27,20 @@ class FragmentCuestionario : Fragment() {
     private lateinit var preguntasArrayList: ArrayList<PreguntasModel>
     private lateinit var adaptadorPreguntas: AdaptadorPreguntas
     private val firestore = AppDatabase.getDatabase()?.firestore
-    private val idQuiz = "hcBYmE4It2lsjb1KYD9J"
+    private var idUsuario: String = ""
+    private var idPreguntas : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        idUsuario = requireActivity().getUserId()
+        idPreguntas = arguments?.getString("idQuestions")
         val root = inflater.inflate(R.layout.fragment_cuestionario, container, false)
         recyclerView = root.findViewById(R.id.listaPreguntas)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -51,24 +58,18 @@ class FragmentCuestionario : Fragment() {
 
 
     private fun EventChangeListener() {
-        firestore?.collection("Quiz")?.document("okGsS2gwYYzuUOIOCOOH")?.collection("quiz")?.
-        addSnapshotListener(object : com.google.firebase.firestore.EventListener<QuerySnapshot>{
-            override fun onEvent(
-                value: QuerySnapshot?,
-                error: FirebaseFirestoreException?
-            ) {
-                if (error != null){
+        firestore?.collection("users/${idUsuario}/quizzes/${idPreguntas}/questions")
+            ?.addSnapshotListener { value, error ->
+                if (error != null) {
                     Log.e("Firestore Error", error.message.toString())
-                    return
+                    return@addSnapshotListener
                 }
-                for (dc : DocumentChange in value?.documentChanges!!){
-                    if (dc.type == DocumentChange.Type.ADDED){
+                for (dc: DocumentChange in value?.documentChanges!!) {
+                    if (dc.type == DocumentChange.Type.ADDED) {
                         preguntasArrayList.add(dc.document.toObject(PreguntasModel::class.java))
                     }
                 }
                 adaptadorPreguntas.notifyDataSetChanged()
             }
-
-        })
     }
 }
--- app/src/main/java/dev/manuel/proyectomoviles/ui/fragments/FragmentCuestionarios.kt
+++ app/src/main/java/dev/manuel/proyectomoviles/ui/fragments/FragmentCuestionarios.kt
@@ -6,19 +6,23 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
+import dev.manuel.proyectomoviles.MainActivity
 import dev.manuel.proyectomoviles.R
 import dev.manuel.proyectomoviles.db.AppDatabase
 import dev.manuel.proyectomoviles.getUserId
 import dev.manuel.proyectomoviles.models.CuestionariosModel
+import dev.manuel.proyectomoviles.setUserId
 import dev.manuel.proyectomoviles.ui.fragments.adapters.AdaptadorCuestionarios
 
 
@@ -28,7 +32,7 @@ class FragmentCuestionarios : Fragment() {
     private lateinit var cuestionariosArrayList: ArrayList<CuestionariosModel>
     private lateinit var adaptadorCuestionarios: AdaptadorCuestionarios
     private val firestore = AppDatabase.getDatabase()?.firestore
-    private lateinit var obtenerId: String
+    private var idQuestions : String = "Gae6sz10OEvLf2hKUOCL" //Ayuda
     private var idUsuario: String = ""
 
 
@@ -59,24 +63,13 @@ class FragmentCuestionarios : Fragment() {
             AdaptadorCuestionarios.onItemClickListener {
 
             override fun onItemClick(position: Int) {
-
-                if (position == 0) findNavController().navigate(R.id.fragmentCuestionario) //Moverse entre pantallas
+                val bundle = bundleOf("idQuestions" to idQuestions)
+               findNavController().navigate(R.id.fragmentCuestionario, bundle) //Moverse entre pantallas
             }
         })
 
         return root
     }
-
-    fun getAllDocumentsUsuarios() {
-        var id: String
-        firestore?.collection("Quiz")?.get()?.addOnSuccessListener { resultado ->
-            for (documentos in resultado) {
-                id = documentos.id
-                println(documentos.id)
-            }
-        }
-    }
-
 
     private fun EventChangeListener() {
         firestore?.collection("users/${idUsuario}/quizzes")
